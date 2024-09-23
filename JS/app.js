633661ece5307c3902cabeec8f8fa511f38c4a22
rakib1514 <rkrakib1514@outlook.com>
2024-09-23 13:30:37 +0600

    history updater function added

--- JS/app.js
+++ JS/app.js
@@ -1,24 +1,34 @@
 const walletBalance = nodeCall("#wallet-balance");
 const noakhaliDonateBtn = nodeCall("#noakhali-donate");
 
+noakhaliDonateBtn.addEventListener("click", function () {
+  const noakhaliCollection = nodeCall("#noakhali-total");
+  const noakhaliInput = nodeCall("#noakhali-input");
+//   const inputValueFloat = parseFloat(noakhaliInput.value);
+  const inputValue = Number(noakhaliInput.value);
+  const walletBalanceNum = parseFloat(walletBalance.innerText);
 
-
-
-
-noakhaliDonateBtn.addEventListener("click", function(){
-    const noakhaliCollection = nodeCall("#noakhali-total");
-    const noakhaliInput = nodeCall("#noakhali-input");
-    const inputValue = Number(noakhaliInput.value);
+  //   validation
+  if (
+    isNaN(inputValue) ||
     
-    const walletBalanceNum = parseFloat(walletBalance.innerText);
-    
-    if (isNaN(inputValue) || inputValue < 0 || walletBalanceNum < 1 || inputValue > walletBalanceNum ) {
-        alert(`Transection Failed. You given "${noakhaliInput.value}" Please give currect Amount`);
-        return;  
-    }
-    
-    updateTotalCollection(noakhaliInput, noakhaliCollection);
-    my_modal_5.showModal()
+    inputValue < 1 ||
+    walletBalanceNum < 1 ||
+    inputValue > walletBalanceNum
+  ) {
+    alert(
+      `Transection Failed. You given "${noakhaliInput.value}" Please give currect Amount`
+    );
+    return;
+  }
+
+  //   all the calculation
+  updateTotalCollection(noakhaliInput, noakhaliCollection);
+  alert("Congratulation");
+
+  // TODO:  modal call
+  //   my_modal_5.showModal();
+
+historyUpdater(inputValue, nodeCall('#noakhali-heading').innerText)
+
 });
-
-
--- JS/utils.js
+++ JS/utils.js
@@ -1,13 +1,29 @@
-function nodeCall(quarySelector){
-    return document.querySelector(quarySelector);
-    
+function nodeCall(quarySelector) {
+  return document.querySelector(quarySelector);
 }
 
 // function for calculate and update zonal collection;
-function updateTotalCollection (input,total) {
-    total.innerText  = parseFloat(input.value) + parseFloat(total.innerText);
-    walletBalance.innerText = parseFloat(walletBalance.innerText) - parseFloat(input.value);
-    input.value = '';
-  }
+function updateTotalCollection(input, total) {
+  total.innerText = parseFloat(input.value) + parseFloat(total.innerText);
+  walletBalance.innerText =
+    parseFloat(walletBalance.innerText) - parseFloat(input.value);
+  input.value = "";
+}
 
-
+// History Update function
+function historyUpdater(amount, headingText) {
+  let currentTime = new Date();
+  const historySec = nodeCall("#history");
+  const newTransec = document.createElement("div");
+  newTransec.innerHTML = `
+    <div class="border border-gray-300 p-4 rounded-lg mb-4">
+        <h3 class="text-xl font-bold">
+            ${amount} Taka is Donated for ${headingText}
+        </h3>
+        <p class="text-gray-500">
+            ${currentTime.toLocaleString()}
+        </p>
+    </div>
+  `;
+  historySec.appendChild(newTransec);
+}
