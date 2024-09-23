7cc9f218c4315fdabd6c1b91eb35738d8c3b3dac
rakib1514 <rkrakib1514@outlook.com>
2024-09-23 16:16:25 +0600

    feni donation js added

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,6 @@
 const walletBalance = nodeCall("#wallet-balance");
 const noakhaliDonateBtn = nodeCall("#noakhali-donate");
-
+const feniDonateBtn = nodeCall('#feni-donate');
 
 // Donation and History Toggle
 nodeCall('#btn-history').addEventListener('click', function(){
@@ -46,4 +46,33 @@ noakhaliDonateBtn.addEventListener("click", function () {
   //   my_modal_5.showModal();
 
   historyUpdater(inputValue, nodeCall("#noakhali-heading").innerText);
+});
+
+feniDonateBtn.addEventListener("click", function () {
+  const feniCollection = nodeCall("#feni-total");
+  const feniInput = nodeCall("#feni-input");
+  const inputValue = Number(feniInput.value);
+  const walletBalanceNum = parseFloat(walletBalance.innerText);
+
+  //   validation
+  if (
+    isNaN(inputValue) ||
+    inputValue < 1 ||
+    walletBalanceNum < 1 ||
+    inputValue > walletBalanceNum
+  ) {
+    alert(
+      `Transection Failed. You given "${feniInput.value}" Please give currect Amount`
+    );
+    return;
+  }
+
+  //   all the calculation
+  updateTotalCollection(feniInput, feniCollection);
+  alert("Congratulation");
+
+  // TODO:  modal call
+  //   my_modal_5.showModal();
+
+  historyUpdater(inputValue, nodeCall("#feni-heading").innerText);
 });
